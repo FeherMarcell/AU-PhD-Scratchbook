56c8a2f3437350ca3db7a8ff5adb10a67df49aed
Marcell <sw0rdf1sh@eng.au.dk>
2019-10-09 16:45:24 +0200

    Hamming 15,11 also supported

--- hamming.py
+++ hamming.py
@@ -42,14 +42,14 @@ def _mul_mat_vec(matrix, vector):
 
 # One possible generator matrix for Hamming(7,4)
 # This moves the 3 parity bits to the front of the codeword
-GENERATOR = [
+_GENERATOR_3 = [
     [0,1,1, 1,0,0,0],
     [1,0,1, 0,1,0,0],
     [1,1,0, 0,0,1,0],
     [1,1,1, 0,0,0,1]
 ]
 
-_GENERATOR = [
+GENERATOR_3 = [
     [1,0,0,0, 1,0,1],
     [0,1,0,0, 1,1,1],
     [0,0,1,0, 1,1,0],
@@ -57,32 +57,73 @@ _GENERATOR = [
 ]
 
 # The corresponding parity check matrix
-PARITY_CHECK = [
+_PARITY_CHECK_3 = [
     [1,0,0, 0,1,1,1],
     [0,1,0, 1,0,1,1],
     [0,0,1, 1,1,0,1]
 ]
 
-_PARITY_CHECK = [
+PARITY_CHECK_3 = [
     [1,1,1,0, 1,0,0],
     [0,1,1,1, 0,1,0],
     [1,1,0,1, 0,0,1]
 ] 
-def hamming_encode(message):
-    if len(message) != 4:
-        raise ValueError("Message must be 4 bits!")
 
+
+GENERATOR_4 = [
+    [1,0,0,0,0,0,0,0,0,0,0, 1,1,0,0],
+    [0,1,0,0,0,0,0,0,0,0,0, 1,0,1,0],
+    [0,0,1,0,0,0,0,0,0,0,0, 0,1,1,0],
+    [0,0,0,1,0,0,0,0,0,0,0, 1,1,1,0],
+    [0,0,0,0,1,0,0,0,0,0,0, 1,0,0,1],
+    [0,0,0,0,0,1,0,0,0,0,0, 0,1,0,1],
+    [0,0,0,0,0,0,1,0,0,0,0, 1,1,0,1],
+    [0,0,0,0,0,0,0,1,0,0,0, 0,0,1,1],
+    [0,0,0,0,0,0,0,0,1,0,0, 1,0,1,1],
+    [0,0,0,0,0,0,0,0,0,1,0, 0,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,1, 1,1,1,1]
+]
+
+PARITY_CHECK_4 = [
+    [1,1,0,1,1,0,1,0,1,0,1, 1,0,0,0],
+    [1,0,1,1,0,1,1,0,0,1,1, 0,1,0,0],
+    [0,1,1,1,0,0,0,1,1,1,1, 0,0,1,0],
+    [0,0,0,0,1,1,1,1,1,1,1, 0,0,0,1]
+] 
+
+
+def hamming_encode(message):
     # Encoding is simply multiplying the message with the generator matrix
-    return _mul_vec_mat(message, GENERATOR)
+
+    generator_matrix = None
+    if len(message) == 4:
+        generator_matrix = GENERATOR_3
+        
+    elif len(message) == 11:
+        generator_matrix = GENERATOR_4
+    
+    else:
+        raise ValueError("Message must be 4 or 11 bits!")
+
+    return _mul_vec_mat(message, generator_matrix)
 #
 
 def hamming_decode(codeword):
     # Decode a potentially noisy codeword received after transmission
     # The result will be correct if 0 or 1 bit of the codeword is corrupted.
 
-    # Generate the syndrome vector
-    syndrome = _mul_mat_vec(PARITY_CHECK, codeword)
+    # Select which parity matrix to use 
+    parity_matrix = None
+    if len(codeword) == 7:
+        parity_matrix = PARITY_CHECK_3
+    elif len(codeword) == 15:
+        parity_matrix = PARITY_CHECK_4
+    else:
+        raise ValueError("Codeword must be 7 or 15 bits!")
     
+    # Generate the syndrome vector
+    syndrome = _mul_mat_vec(parity_matrix, codeword)
+
     # Find which bit was flipped using the syndrome, and flip it back
     fixed_codeword = hamming_fix_with_syndrome(codeword, syndrome)
     
@@ -92,22 +133,33 @@ def hamming_decode(codeword):
     # left side, the decoded data is at the end of the fixed 
     # codeword, if the identity is on the right the data bits 
     # are in the beginning
-    data_bits = fixed_codeword[:4] if GENERATOR[0][:4] == [1,0,0,0] else fixed_codeword[3:]
+    data_bits = fixed_codeword[:4] if GENERATOR_3[0][:4] == [1,0,0,0] else fixed_codeword[3:]
     
+    data_bits = fixed_codeword[:4] if parity_matrix == PARITY_CHECK_3 else fixed_codeword[:11]
     return (data_bits, syndrome)
 #
 
 def hamming_fix_with_syndrome(codeword, syndrome):
 
-    if syndrome == [0,0,0]:
+
+    if syndrome == [0,0,0] or syndrome == [0,0,0,0]:
         # No error in transmission
         return codeword
+    
+    # Select which parity matrix to use 
+    parity_matrix = None
+    if len(codeword) == 7:
+        parity_matrix = PARITY_CHECK_3
+    elif len(codeword) == 15:
+        parity_matrix = PARITY_CHECK_4
+    else:
+        raise ValueError("Codeword must be 7 or 15 bits!")
     
     # Find which column of the parity check matrix is identical to the syndrome
     # TODO instead of searching for the parity column, we could precompute 
     # a lookup table that has the correct flipped bit index for every possible syndrome
-    for i in range(len(PARITY_CHECK[0])):
-        parity_col = [PARITY_CHECK[j][i] for j in range(len(PARITY_CHECK))]
+    for i in range(len(parity_matrix[0])):
+        parity_col = [parity_matrix[j][i] for j in range(len(parity_matrix))]
 
         if parity_col == syndrome:
             #print("Syndrome: %s, Error bit: %d" % (syndrome, i))
@@ -119,7 +171,7 @@ def hamming_fix_with_syndrome(codeword, syndrome):
     raise ValueError("Failed to fix codeword, no fix found for syndrome {}".format(syndrome))
 #
 
-def test_hamming():
+def test_hamming_code(length):
     """
     Tests Hamming encode and decode for every 4-bit binary sequence, 
     simulating every possible single bit error between encode and decode.
@@ -128,7 +180,20 @@ def test_hamming():
     """
 
     # Generate every possible 4-bit binary message (16 messages)
-    every_message = [ [i,j,k,l] for i in (0,1) for j in (0,1) for k in (0,1) for l in (0,1) ]
+    if length == 4:
+        every_message = [ [i,j,k,l] for i in (0,1) for j in (0,1) for k in (0,1) for l in (0,1) ]
+    
+    elif length == 11:
+        # Generate a set of random 11-bit messages
+        every_message = []
+        for i in range(100):
+            message = [0] * 11
+            for b in range(len(message)):
+                message[b] = random.getrandbits(1)
+            every_message.append(message.copy())
+    
+    print("Messages to test: %d" % len(every_message))
+
 
     for message in every_message:
 
@@ -167,6 +232,15 @@ def test_hamming():
 
     print("Every possible 4-bit message with 1 bit error was corrected.")
 
+
+def test_hamming():
+
+    # Test Hamming (7,4)
+    test_hamming_code(4)
+
+    # Test Hamming (15,11)
+    test_hamming_code(11)
+#
 
 
 # What to run when this python file is invoked (not imported somewhere else)
